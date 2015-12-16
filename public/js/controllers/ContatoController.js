26a71f121d3c420912d147982a19cfad2889c1ad
Maykon Luis Capellari <maykonluiscapellari@gmail.com>
2015-12-16 01:12:54 -0200

    add tests and update karma config

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,10 +1,12 @@
 angular.module('contatooh').controller('ContatoController',
   function($scope, $routeParams, Contato) {
 
-    function buscaContatos(contatoId){
+    function buscaContatos(contatoId) {
       $scope.contato = {};
-      if(contatoId){
-        var promise = Contato.get({id: contatoId}).$promise;
+      if (contatoId) {
+        var promise = Contato.get({
+          id: contatoId
+        }).$promise;
         promise
           .then(function(contato) {
             $scope.contato = contato;
@@ -16,7 +18,7 @@ angular.module('contatooh').controller('ContatoController',
               texto: 'Não foi possível obter a lista'
             };
           });
-      }else{
+      } else {
         $scope.contato = new Contato();
       }
     }
@@ -24,15 +26,20 @@ angular.module('contatooh').controller('ContatoController',
     $scope.salva = function() {
       $scope.contato.$save()
         .then(function() {
-          $scope.mensagem = {texto: 'Salvo com sucesso'};
+          $scope.mensagem = {
+            texto: 'Salvo com sucesso'
+          };
           $scope.contato = new Contato();
+          $scope.$broadcast('contatoSalvo');
         })
         .catch(function(erro) {
-          $scope.mensagem = {texto: 'Não foi possível salvar'};
+          $scope.mensagem = {
+            texto: 'Não foi possível salvar'
+          };
         });
     };
 
-    $scope.init = function(){
+    $scope.init = function() {
       buscaContatos($routeParams.contatoId);
     };
     $scope.init();
